d16a5b51b5acca312d574035f5fa3ef61717730b
koukikubo <kouki.xx.0817@gmail.com>
2026-05-17 15:43:51 +0900

    ✨ StandardListMasterSerializerおよびStandardMasterSerializerクラスを追加し、リソースのJSON表現を実装

---
+++ backend/app/serializers/standard_master_serializer.rb
@@ -0,0 +1,16 @@
+class StandardMasterSerializer < ApplicationSerializer
+
+  def as_json
+    {
+      id: @resource.id,
+      code: @resource.code,
+      name: @resource.name,
+      description: @resource.description,
+      active: @resource.active,
+      position: @resource.position,
+      items: @resource.standard_list_masters.ordered.map do |item|
+        StandardListMasterSerializer.new(item).as_json
+      end
+    }
+  end
+end
