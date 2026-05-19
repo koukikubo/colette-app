5f1f29fb7400b6855a37c72ee422dc73fee78c8f
koukikubo <kouki.xx.0817@gmail.com>
2026-05-20 08:36:11 +0900

    ✨ StandardMastersControllerおよびStandardListMastersControllerを追加し、リソースのJSON表現を実装

---
+++ backend/app/controllers/api/v1/standard_masters_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::StandardMastersController < Api::V1::BaseController
+  def index
+    keyword = params[:query].presence || params[:q].presence
+    standard_masters =
+      StandardMaster
+      .search(query: keyword, active: params[:active])
+      .includes(:standard_list_masters)
+      .ordered
+    render_success(
+      data: standard_masters.map do |standard_master|
+        Api::V1::StandardMasterSerializer.new(standard_master).as_json
+      end
+    )  
+  end
+
+  def show
+    standard_master =
+      Api::V1::StandardMaster
+      .includes(:standard_list_masters)
+      .find_by!(code: params[:code])
+    render_success(
+      data: Api::V1::StandardMasterSerializer.new(standard_master).as_json
+    )
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,17 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "health", to: "health#index"
+      resources :standard_masters, only: %i[index show], param: :code do
+        collection do
+          get :next_code
+          get :count
+        end
+
+        resources :standard_list_masters,
+          path: "items",
+          as: "items",
+          only: [:index, :show, :create, :update, :destroy]
+      end 
     end
   end
-
 end
