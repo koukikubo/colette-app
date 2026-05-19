8097c159fe3cb4aeae6c23e323b6b5f860425648
koukikubo <kouki.xx.0817@gmail.com>
2026-05-20 08:37:02 +0900

    ✨ 新しいシリアライザ（StaffSerializer、StandardMasterSerializer、StandardListMasterSerializer）を追加し、リソースのJSON表現を実装

---
+++ backend/app/serializers/api/v1/staff_serializer.rb
@@ -0,0 +1,16 @@
+module Api
+  module V1
+    class StaffSerializer < ApplicationSerializer
+      # def as_json
+      #   {
+      #     id: @resource.id,
+      #     code: @resource.code,
+      #     label: @resource.label,
+      #     description: @resource.description,
+      #     position: @resource.position,
+      #     active: @resource.active
+      #   }
+      # end
+    end
+  end
+end
