20fa7ac519aab2aa1988c55fbe6ededb9ef5d6ff
koukikubo <kouki.xx.0817@gmail.com>
2026-05-24 16:28:36 +0900

    ✨ スタッフおよびスタッフマスターのモデルスペックを追加し、バリデーションとメソッドのテストを実装

--- backend/spec/models/staff_master_spec.rb
+++ backend/spec/models/staff_master_spec.rb
@@ -1,5 +1,162 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe StaffMaster, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "有効な属性で作成できること" do
+      staff_master = described_class.new(
+        code: "STF001",
+        name: "オーナー",
+        role_code: "owner",
+        employment_started_on: Date.current
+      )
+
+      expect(staff_master).to be_valid
+    end
+
+    it "codeが必須であること" do
+      staff_master = described_class.new(
+        code: nil,
+        name: "オーナー",
+        role_code: "owner",
+        employment_started_on: Date.current
+      )
+
+      expect(staff_master).to be_invalid
+      expect(staff_master.errors[:code]).to be_present
+    end
+
+    it "codeが一意であること" do
+      described_class.create!(
+        code: "STF001",
+        name: "オーナー",
+        role_code: "owner",
+        employment_started_on: Date.current
+      )
+
+      duplicate = described_class.new(
+        code: "STF001",
+        name: "別スタッフ",
+        role_code: "operator",
+        employment_started_on: Date.current
+      )
+
+      expect(duplicate).to be_invalid
+      expect(duplicate.errors[:code]).to be_present
+    end
+
+    it "nameが必須であること" do
+      staff_master = described_class.new(
+        code: "STF001",
+        name: nil,
+        role_code: "owner",
+        employment_started_on: Date.current
+      )
+
+      expect(staff_master).to be_invalid
+      expect(staff_master.errors[:name]).to be_present
+    end
+
+    it "role_codeが必須であること" do
+      staff_master = described_class.new(
+        code: "STF001",
+        name: "オーナー",
+        role_code: nil,
+        employment_started_on: Date.current
+      )
+
+      expect(staff_master).to be_invalid
+      expect(staff_master.errors[:role_code]).to be_present
+    end
+
+    it "employment_started_onが必須であること" do
+      staff_master = described_class.new(
+        code: "STF001",
+        name: "オーナー",
+        role_code: "owner",
+        employment_started_on: nil
+      )
+
+      expect(staff_master).to be_invalid
+      expect(staff_master.errors[:employment_started_on]).to be_present
+    end
+  end
+
+  describe "associations" do
+    it "staffを1件紐付けられること" do
+      staff_master = described_class.create!(
+        code: "STF001",
+        name: "オーナー",
+        role_code: "owner",
+        employment_started_on: Date.current
+      )
+
+      staff = Staff.create!(
+        staff_master: staff_master,
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      expect(staff_master.staff).to eq(staff)
+    end
+  end
+
+describe "scopes" do
+  it "activeはretired_onがnilのスタッフマスタを返すこと" do
+    active_staff = described_class.create!(
+      code: "STF001",
+      name: "在籍スタッフ",
+      role_code: "owner",
+      employment_started_on: Date.current,
+      retired_on: nil
+    )
+
+    described_class.create!(
+      code: "STF002",
+      name: "退職スタッフ",
+      role_code: "operator",
+      employment_started_on: Date.current,
+      retired_on: Date.current
+    )
+
+    expect(described_class.active).to include(active_staff)
+    expect(described_class.active.count).to eq(1)
+  end
+
+  it "retired_onはretired_onが存在するスタッフマスタを返すこと" do
+    described_class.create!(
+      code: "STF001",
+      name: "在籍スタッフ",
+      role_code: "owner",
+      employment_started_on: Date.current,
+      retired_on: nil
+    )
+
+    retired_staff = described_class.create!(
+      code: "STF002",
+      name: "退職スタッフ",
+      role_code: "operator",
+      employment_started_on: Date.current,
+      retired_on: Date.current
+    )
+
+    expect(described_class.retired_on).to include(retired_staff)
+    expect(described_class.retired_on.count).to eq(1)
+  end
+end
+
+  describe "#active?" do
+    it "retired_onがnilの場合trueを返すこと" do
+      staff_master = described_class.new(retired_on: nil)
+
+      expect(staff_master.active?).to eq(true)
+    end
+  end
+
+  describe "#retired?" do
+    it "retired_onが存在する場合trueを返すこと" do
+      staff_master = described_class.new(retired_on: Date.current)
+
+      expect(staff_master.retired?).to eq(true)
+    end
+  end
 end
--- backend/spec/models/staff_spec.rb
+++ backend/spec/models/staff_spec.rb
@@ -1,5 +1,153 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Staff, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def build_staff_master(code: "STF001", retired_on: nil)
+    StaffMaster.new(
+      code: code,
+      name: "オーナー",
+      role_code: "owner",
+      employment_started_on: Date.current,
+      retired_on: retired_on
+    )
+  end
+
+  describe "validations" do
+    it "有効な属性で作成できること" do
+      staff_master = build_staff_master
+
+      staff = described_class.new(
+        staff_master: staff_master,
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      expect(staff).to be_valid
+    end
+
+    it "staff_masterが必須であること" do
+      staff = described_class.new(
+        staff_master: nil,
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      expect(staff).to be_invalid
+      expect(staff.errors[:staff_master]).to be_present
+    end
+
+    it "staff_master_idが一意であること" do
+      staff_master = StaffMaster.create!(
+        code: "STF001",
+        name: "オーナー",
+        role_code: "owner",
+        employment_started_on: Date.current
+      )
+
+      described_class.create!(
+        staff_master: staff_master,
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      duplicate = described_class.new(
+        staff_master: staff_master,
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      expect(duplicate).to be_invalid
+      expect(duplicate.errors[:staff_master_id]).to be_present
+    end
+
+    it "failed_attemptsは0以上であること" do
+      staff = described_class.new(
+        staff_master: build_staff_master,
+        password: "password",
+        password_confirmation: "password",
+        failed_attempts: -1
+      )
+
+      expect(staff).to be_invalid
+      expect(staff.errors[:failed_attempts]).to be_present
+    end
+
+    it "login_enabledはtrueまたはfalseであること" do
+      staff = described_class.new(
+        staff_master: build_staff_master,
+        password: "password",
+        password_confirmation: "password",
+        login_enabled: nil
+      )
+
+      expect(staff).to be_invalid
+      expect(staff.errors[:login_enabled]).to be_present
+    end
+  end
+
+  describe "has_secure_password" do
+    it "正しいpasswordで認証できること" do
+      staff = described_class.create!(
+        staff_master: StaffMaster.create!(
+          code: "STF001",
+          name: "オーナー",
+          role_code: "owner",
+          employment_started_on: Date.current
+        ),
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      expect(staff.authenticate("password")).to eq(staff)
+    end
+
+    it "誤ったpasswordでは認証できないこと" do
+      staff = described_class.create!(
+        staff_master: StaffMaster.create!(
+          code: "STF001",
+          name: "オーナー",
+          role_code: "owner",
+          employment_started_on: Date.current
+        ),
+        password: "password",
+        password_confirmation: "password"
+      )
+
+      expect(staff.authenticate("wrong")).to eq(false)
+    end
+  end
+
+  describe "#login_allowed?" do
+    it "login_enabledがtrueかつ退職していない場合trueを返すこと" do
+      staff_master = build_staff_master(retired_on: nil)
+
+      staff = described_class.new(
+        staff_master: staff_master,
+        login_enabled: true
+      )
+
+      expect(staff.login_allowed?).to eq(true)
+    end
+
+    it "login_enabledがfalseの場合falseを返すこと" do
+      staff_master = build_staff_master(retired_on: nil)
+
+      staff = described_class.new(
+        staff_master: staff_master,
+        login_enabled: false
+      )
+
+      expect(staff.login_allowed?).to eq(false)
+    end
+
+    it "退職済みの場合falseを返すこと" do
+      staff_master = build_staff_master(retired_on: Date.current)
+
+      staff = described_class.new(
+        staff_master: staff_master,
+        login_enabled: true
+      )
+
+      expect(staff.login_allowed?).to eq(false)
+    end
+  end
 end
