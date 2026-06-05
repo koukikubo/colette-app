a1373ba004298d5b85e84c2928da255576ba8dd7
koukikubo <kouki.xx.0817@gmail.com>
2026-06-06 08:54:41 +0900

    feat: StandardMastersControllerにcreate、update、destroyメソッドを追加し、標準マスタの管理機能を強化

--- backend/app/controllers/api/v1/standard_masters_controller.rb
+++ backend/app/controllers/api/v1/standard_masters_controller.rb
@@ -21,10 +21,68 @@ class Api::V1::StandardMastersController < Api::V1::BaseController
       StandardMaster
       .includes(:standard_list_masters)
       .find_by!(code: params[:code])
+
     render_success(
       data: {
         standard_master: Api::V1::StandardMasterSerializer.new(standard_master).as_json
       }
     )
   end
+
+  def create
+    standard_master = StandardMaster.new(standard_master_params)
+
+    standard_master.position =
+      StandardMaster.maximum(:position).to_i + 1
+
+    standard_master.save!
+
+    render_success(
+      data: {
+        standard_master:
+          Api::V1::StandardMasterSerializer.new(standard_master).as_json
+      },
+      status: :created
+    )
+  end
+
+  def update
+    standard_master = StandardMaster.find_by!(code: params[:code])
+
+    standard_master.update!(standard_master_params)
+
+    render_success(
+      data: {
+        standard_master:
+          Api::V1::StandardMasterSerializer.new(standard_master).as_json
+      }
+    )
+  end
+
+  def destroy
+    standard_master = StandardMaster.find_by!(code: params[:code])
+
+    standard_master.update!(active: false)
+
+    render_success(
+      data: {
+        message: "標準マスタを無効化しました。",
+        standard_master:
+          Api::V1::StandardMasterSerializer.new(standard_master).as_json
+          .new(standard_master).as_json 
+      }
+    )
+  end
+
+  private
+
+  def standard_master_params
+    permitted_params = params.expect(standard_master: %i[code name description active])
+    permitted_params.require(:standard_master).permit(:code, :name, :description, :active)
+
+    permitted_params
+  end
+
+
 end
+
