3ac06e56cac492ef5eb49a7240fdf93c8ca754e1
koukikubo <kouki.xx.0817@gmail.com>
2026-06-15 19:41:15 +0900

    ✨ FactoryBotの設定を改善: staff_masterとstaffのファクトリを追加し、テストデータの生成を強化

--- backend/spec/factories/staffs.rb
+++ backend/spec/factories/staffs.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :staff do
-    
+    association :staff_master
+
+    password { "Password123!" }
+    password_confirmation { "Password123!" }
+
+    login_enabled { true }
+    failed_attempts { 0 }
+    locked_at { nil }
+    last_logged_in_at { nil }
   end
 end
