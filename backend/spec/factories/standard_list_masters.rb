aa239eec7822aceb6f91921c57f67a57b1b9fb18
koukikubo <kouki.xx.0817@gmail.com>
2026-06-25 16:49:42 +0900

    ✨ standard_list_masterおよびstandard_masterファクトリを整理

--- backend/spec/factories/standard_list_masters.rb
+++ backend/spec/factories/standard_list_masters.rb
@@ -1,9 +1,20 @@
+# backend/spec/factories/standard_list_masters.rb
+
 FactoryBot.define do
   factory :standard_list_master do
     association :standard_master
 
-    sequence(:label) { |number| "選択肢#{number}" }
-    sequence(:position) { |number| number }
+    sequence(:code) do |number|
+      "CODE#{number}"
+    end
+
+    sequence(:label) do |number|
+      "選択肢#{number}"
+    end
+
+    sequence(:position) do |number|
+      number
+    end
 
     description { "選択肢コードの説明" }
     active { true }
