2ccd5a5e20d83d7a4e09ae109268835353a948b3
koukikubo <kouki.xx.0817@gmail.com>
2026-06-26 13:22:59 +0900

    ✨ 予約テーブルマスタページを新規作成

---
+++ frontend/app/(protected)/restaurant-masters/page.tsx
@@ -0,0 +1,5 @@
+import { RestaurantTableMasterPage } from "@/features/restaurant-tables/components/RestaurantTableMasterPage";
+
+export default function RestaurantTablesPage() {
+  return <RestaurantTableMasterPage />;
+}
