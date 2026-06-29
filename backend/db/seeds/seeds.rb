adcfe4d87866919a0ea7c945d252b0ea6fe780a8
koukikubo <kouki.xx.0817@gmail.com>
2026-06-29 14:22:18 +0900

    ✨ スタッフマスタのシードデータを定義し、初期スタッフアカウントの作成処理を追加

--- backend/db/seeds/seeds.rb
+++ backend/db/seeds/seeds.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
-
+load Rails.root.join("db/seeds/staffs.rb")
 load Rails.root.join("db/seeds/staffs.rb", "db/seeds/standard_masters.rb")
 load Rails.root.join("db/seeds/restaurant_masters.rb")
