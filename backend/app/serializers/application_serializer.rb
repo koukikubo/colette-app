e24281dfed4d217b9e6ebc612a24243dd39c350c
koukikubo <kouki.xx.0817@gmail.com>
2026-07-10 13:57:58 +0900

    ✨ ApplicationSerializerにリソースコレクションメソッドとスタッフ情報シリアライズメソッドを追加

--- backend/app/serializers/application_serializer.rb
+++ backend/app/serializers/application_serializer.rb
@@ -1,8 +1,26 @@
 class ApplicationSerializer
   # シリアライザの基底クラス
   attr_reader :resource
-
+  # シリアライズ対象のリソースを返す
+  def self.collection(resources)
+    resources.map do |resource|
+      new(resource).as_json
+    end
+  end
+  # シリアライズ対象のリソースを返す
   def initialize(resource)
     @resource = resource
+  end
+
+  private
+  # スタッフ情報をシリアライズする
+  def serialize_staff(staff)
+    return nil if staff.blank?
+
+    {
+      id: staff.id,
+      staff_master_id: staff.staff_master_id,
+      name: staff.staff_master&.name
+    }
   end
 end
