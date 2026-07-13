7d5547fc860fc45630860cde472a507bc6c67126
koukikubo <kouki.xx.0817@gmail.com>
2026-07-13 14:13:35 +0900

    ✨ 予約キャンセルおよび復元機能のRSpecテストを追加

--- backend/spec/factories/reservations.rb
+++ backend/spec/factories/reservations.rb
@@ -1,13 +1,46 @@
 FactoryBot.define do
   factory :reservation do
-    customer { nil }
-    starts_at { "2026-07-01 10:18:24" }
-    ends_at { "2026-07-01 10:18:24" }
-    guest_count { 1 }
-    reservation_status_code { "MyString" }
-    reservation_route_code { "MyString" }
-    memo { "MyText" }
-    canceled_at { "2026-07-01 10:18:24" }
-    lock_version { 1 }
+    sequence(:reservation_name) do |number|
+      "予約者#{number}"
+    end
+
+    sequence(:reservation_phone_number) do |number|
+      format("090%08d", number)
+    end
+
+    starts_at do
+      Time.zone.local(
+        Time.zone.today.year,
+        Time.zone.today.month,
+        Time.zone.today.day,
+        18,
+        0
+      )
+    end
+
+    ends_at { starts_at + 2.hours }
+
+    guest_count { 2 }
+
+    association :requested_restaurant_master_type,
+                factory: :standard_list_master
+
+    association :reservation_status,
+                factory: :standard_list_master
+
+    association :created_by_staff,
+                factory: :staff
+
+    updated_by_staff { created_by_staff }
+
+    canceled_at { nil }
+
+    trait :canceled do
+      canceled_at { Time.current }
+    end
+
+    trait :details_confirmed do
+      details_confirmed_at { Time.current }
+    end
   end
 end
