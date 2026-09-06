907e2261b77897d3efa0d4f76d3aac8117248ec3
koukikubo <kouki.xx.0817@gmail.com>
2026-09-06 19:37:08 +0900

    test(frontend): 担当者マスタのテストを拡充

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -2,6 +2,14 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
+class ResizeObserverMock implements ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+globalThis.ResizeObserver = ResizeObserverMock;
+
 afterEach(() => {
   cleanup();
 });
